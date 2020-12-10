04e18d512b459b552d6fcfc377350cb7c3242aaa
edwin20100 <Edwin20100@hotmail.es>
2020-12-10 13:07:02 -0600

    Final API Project
    CRUD BASIC API

--- JobBoardApi/Controllers/JobController.cs
+++ JobBoardApi/Controllers/JobController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using JobBoardApi.Models;
+using JobBoardApi.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,11 +9,71 @@ using System.Threading.Tasks;
 
 namespace JobBoardApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("[controller]")]
     [ApiController]
     public class JobController : ControllerBase
     {
 
+        private JobService _jobService;
 
+        [HttpGet(Name = "Get")]
+        public ActionResult<List<Jobs>> Get()
+        {
+            return _jobService.GetAll();
+        }
+
+        [HttpGet("{job}", Name = "GetJob")]
+        public ActionResult<Jobs> Get(string job)
+        {
+            var jobResult = _jobService.Get(job);
+            if (jobResult != null)
+            {
+                return jobResult;
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Jobs job)
+        {
+            var result = _jobService.Insert(job);
+            return CreatedAtRoute("GetJob", new { job = result.Job }, result);
+        }
+
+
+        [HttpPut]
+        [Route("[action]")]
+        public IActionResult Update(Jobs job)
+        {
+            var result = _jobService.Update(job);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return CreatedAtRoute("GetJob", new { job = result.Job }, result);
+        }
+
+
+        [HttpDelete]
+        [Route("[action]/{job}")]
+        public IActionResult Delete(string job)
+        {
+            var jobData = _jobService.Get(job);
+            if (job == null)
+            {
+                return NotFound();
+            }
+
+            _jobService.Remove(jobData);
+
+            return NoContent();
+        }
+
+
+        public JobController(Models.JobBoardContext _context)
+        {
+            this._jobService = new JobService(_context);
+        }
     }
 }
