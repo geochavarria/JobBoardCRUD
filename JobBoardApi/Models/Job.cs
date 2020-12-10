122f1d6881c4b3afa5ec0af9f48f2081be69a414
edwin20100 <Edwin20100@hotmail.es>
2020-12-10 12:19:55 -0600

    Creating Folder Structure - Data Models

---
+++ JobBoardApi/Models/Job.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobBoardApi.Models
+{
+    public class Jobs
+    {
+        [Key]
+        public string Job { get; set; } = Guid.NewGuid().ToString();
+        public string  JobTitle { get; set; }
+        public string Description { get; set; }
+        public DateTime CretedAt { get; set; }
+        public DateTime ExpiresAt { get; set; }    
+    }
+}
---
+++ JobBoardApi/Models/JobBoardContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobBoardApi.Models
+{
+    public class JobBoardContext
+    {
+        
+    }
+}
